d997fa79964eae1e4f4d22636d25372cebedc156
Deumir404 <deumir@yandex.ru>
2024-08-18 23:47:50 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sudoku_helper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            foreach (TextBox textBox in mainGrid.Children.OfType<TextBox>())
+            {
+                textBox.Text = "";
+                Brush brush = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+                textBox.Background = brush;
+
+            }
+        }
+
+        private void Clear_area()
+        {
+            foreach (TextBox textBox in mainGrid.Children.OfType<TextBox>())
+            {
+                Brush brush = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+                textBox.Background = brush;
+            }
+        }
+
+        private void Check_match(string num)
+        {
+            foreach (TextBox textBox in mainGrid.Children.OfType<TextBox>())
+            {
+                if (textBox.Text == num)
+                {
+                    var row = Grid.GetRow(textBox);
+                    var col = Grid.GetColumn(textBox);
+                    Fill_table(row, col);
+                    
+                }
+            }
+        }
+        private void Fill_table(int row, int col)
+        {
+            foreach (TextBox textBox in mainGrid.Children.OfType<TextBox>())
+            {
+                var n_row = Grid.GetRow(textBox);
+                var n_col = Grid.GetColumn(textBox);
+                if (n_row == row || n_col == col)
+                {
+                    Brush brush = new SolidColorBrush(Color.FromArgb(123, 255, 0, 0));
+                    textBox.Background = brush;
+                }
+            }
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            string[] arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+            TextBox? textBox = sender as TextBox;
+            if (!arr.Contains(textBox.Text))
+            {
+                textBox.Text = "";
+            }
+            else
+            {
+                Clear_area();
+                Check_match(textBox.Text);
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string[] arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+            TextBox? textBox = sender as TextBox;
+            if (!arr.Contains(textBox.Text))
+            {
+                textBox.Text = "";
+            }
+            else
+            {
+                Clear_area();
+                Check_match(textBox.Text);
+            }
+        }
+    }
+}
